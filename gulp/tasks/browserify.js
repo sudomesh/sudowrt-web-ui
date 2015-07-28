4f0cac06306e0a59b06f2c5924cbc50659381822
Maxb <maxb.personal@gmail.com>
2015-07-28 08:04:06 -0700

    Using watchify to improve reload times for browserify compile.

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -5,6 +5,7 @@ var gulpif       = require('gulp-if');
 var uglify       = require('gulp-uglify');
 var notify       = require('gulp-notify');
 var riotify      = require('riotify');
+var watchify     = require('watchify');
 var handleErrors = require('../util/handleErrors');
 var source       = require('vinyl-source-stream');
 var buffer       = require('vinyl-buffer');
@@ -13,23 +14,30 @@ var sourcemaps   = require('gulp-sourcemaps');
 var output = argv.prod ? 'bundle.min.js' : 'bundle.js';
 
 gulp.task('browserify', function() {
-  // set up the browserify instance on a task basis
+
   var b = browserify({
     entries: ['./web-src/js/main.js'],
     debug: !argv.prod,
-    // defining transforms here will avoid crashing your stream?
-    // transform: [riotify, {type: 'es6', ext: 'tag'}]
   });
 
-  return b.transform(riotify, {type: 'es6', ext: 'tag'})
-    .bundle()
-    .on('error', notify.onError("Error: <%= error.message %>"))
-    .pipe(source(output))
-    .pipe(buffer())
-    .pipe(gulpif(argv.prod, uglify()))
-    .pipe(gulpif(!argv.prod, sourcemaps.init({loadMaps: true})))
-    .pipe(gulpif(!argv.prod, sourcemaps.write('./')))
-    .on("error", notify.onError("Error: <%= error.message %>"))
-    .pipe(gulp.dest('./web-build/js/'))
-    .pipe(notify());
+  var bundle = function() {
+    return b.transform(riotify, {type: 'es6', ext: 'tag'})
+      .bundle()
+      .on('error', notify.onError("Error: <%= error.message %>"))
+      .pipe(source(output))
+      .pipe(buffer())
+      .pipe(gulpif(argv.prod, uglify()))
+      .pipe(gulpif(!argv.prod, sourcemaps.init({loadMaps: true})))
+      .pipe(gulpif(!argv.prod, sourcemaps.write('./')))
+      .on("error", notify.onError("Error: <%= error.message %>"))
+      .pipe(gulp.dest('./web-build/js/'))
+      .pipe(notify());
+  }
+
+  if (argv.prod) {
+    b = watchify(b);
+    b.on('update', bundle);
+  }
+
+  return bundle();
 });
